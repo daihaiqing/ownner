819c195006f88f549c35756f2ddebb92ba3879e2
haiqing.dai <1083197739@qq.com>
2017-11-23 00:20:53 +0800

    添加marko

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,14 @@
-"use strict";
+'use strict';
 
-var a = 1;
+var str = function str(num, end) {
+	var a = [num];
+
+	for (var i = +num - 1; i > +end - 1; i--) {
+		a.push(i);
+		a.unshift(i);
+	}
+
+	return a.join('');
+};
+
+str(6, 1);
